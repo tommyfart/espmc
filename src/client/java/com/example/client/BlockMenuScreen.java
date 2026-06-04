76622fe9f992f723769e896852d7e2d87f698af4
tommyfart <135668188+tommyfart@users.noreply.github.com>
2026-06-03 21:48:32 -0600

    Add BlockMenuScreen class for block selection

---
+++ src/client/java/com/example/client/BlockMenuScreen.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+public class BlockMenuScreen extends Screen {
+
+    protected BlockMenuScreen() {
+        super(Text.literal("Choose blocks"));
+    }
+
+}
