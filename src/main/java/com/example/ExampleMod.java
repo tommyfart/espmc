fb7759c94ef88e942a3b1456debba6dc5c87fdd2
tommyfart <135668188+tommyfart@users.noreply.github.com>
2026-06-03 20:19:15 -0600

    Fix formatting and remove unnecessary imports

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -13,19 +13,4 @@ public class ExampleMod implements ModInitializer {
     public void onInitialize() {
         LOGGER.info("Loaded block finder");
     }
-}package com.example;
-
-import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class ExampleMod implements ModInitializer {
-
-    public static final String MOD_ID = "modid";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-    @Override
-    public void onInitialize() {
-        LOGGER.info("Loaded block finder");
-    }
 }
